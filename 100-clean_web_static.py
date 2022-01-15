43dbb8cbd0fec84e3afe00f197aa1da261ea9e8b
JuanSebastianGB <3393@holbertonschool.com>
2022-01-15 16:52:30 +0000

    UPL: Uploading function to delete n old versions

---
+++ 100-clean_web_static.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+deletes out-of-date archives, using the function do_clean
+"""
+from ensurepip import version
+from fabric.api import *
+import os
+
+
+def sort_versions(versions):
+    """Return sorted versions"""
+    return sorted(versions[:])
+
+
+def delete_versions_local(versions):
+    """Deleting from local server"""
+    with lcd("versions"):
+        [local("rm ./{}".format(version)) for version in versions]
+
+
+def delete_versions_remote(number=0):
+    """Deleting from remote servers"""
+    with cd("/data/web_static/releases"):
+        versions = run("ls -rt").split()
+        versions = [
+            version for version in versions if "web_static" in version]
+        [version.pop() for _ in range(number)]
+        [run("rm -rf ./{}".format(version)) for version in versions]
+
+
+def do_clean(number=0):
+    """deletes out of date archives"""
+    number = 1 if int(number) == 0 else int(number)
+    versions = sort_versions(os.listdir("versions"))
+    try:
+        """ excluding version of this list to save them"""
+        [versions.pop() for _ in range(number)]
+        delete_versions_local(versions)
+        delete_versions_remote(number)
+    except BaseException as _:
+        pass
