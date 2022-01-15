5fb5642c263e58b41ff121e67bfd8347a13c292e
JuanSebastianGB <3393@holbertonschool.com>
2022-01-15 17:57:19 +0000

    CHANGIN WONER

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -23,24 +23,33 @@ $config = "server {
 package { 'nginx':
     ensure   => 'present',
     provider => 'apt'
-}
+} ->
+
 file { ['/data/', '/data/web_static/', '/data/web_static/releases/',
         '/data/web_static/releases/test/', '/data/web_static/shared']:
     ensure => 'directory',
-}
+} ->
+
 file {'/data/web_static/releases/test/index.html':
     ensure  => 'present',
-    content => 'Holberton School',
-}
+    content => 'Holberton School\n',
+} ->
+
 file { '/data/web_static/current':
     ensure => 'link',
     target => '/data/web_static/releases/test/',
     force  => yes,
+} ->
+
+exec { 'chown -R ubuntu:ubuntu /data/':
+    path => '/usr/bin/:/usr/local/bin/:/bin/'
+
 }
 file { '/etc/nginx/sites-available/default':
     ensure  => 'present',
     content => $config,
-}
+} ->
+
 service { 'nginx restart':
     require => 'etc/init.d'
 }
