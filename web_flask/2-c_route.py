c7b90613d92b87d94f37d1e7b2743569eb862d3b
JuanSebastianGB <3393@holbertonschool.com>
2022-01-29 13:25:14 +0000

    UPL: Uplaoding task 2, defining c route with an input

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+flask application
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """ Defining index route"""
+    return 'Hello HBNB!'
+
+
+@app.route('/', strtict_slashes=False)
+def hbnb():
+    """ Defining hbnb route"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strtict_slashes=False)
+def c(text):
+    """ Defining c route with an input"""
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+app.run(host='0.0.0.0', port='5000')
