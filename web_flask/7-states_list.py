9d4d53c6dd119b17690de6c1aef20d29a360da11
JuanSebastianGB <3393@holbertonschool.com>
2022-01-30 01:30:39 +0000

    DEL: Deletin blank space

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,9 +2,10 @@
 """
 Stars Flask web application
 """
-from flask import Flask, render_template
 from models import storage
-""" from models import storage """
+from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 
