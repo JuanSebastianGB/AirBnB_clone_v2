c4e4cd8a055a88c070abcfae79383b438c0cf180
JuanSebastianGB <3393@holbertonschool.com>
2022-01-30 01:58:58 +0000

    FIX: Deleting not required information from the print

--- models/state.py
+++ models/state.py
@@ -13,8 +13,10 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
 
+    cities = relationship('City', backref='state', cascade='delete')
+    
     if getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship('City', backref='state', cascade='delete')
+        pass
     else:
         @property
         def cities(self):
