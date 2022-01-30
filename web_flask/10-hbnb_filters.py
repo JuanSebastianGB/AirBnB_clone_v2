ce0f4ef42272345919c76ada96cbb9d8aaed0fa9
JuanSebastianGB <3393@holbertonschool.com>
2022-01-30 04:36:29 +0000

    UPL: Starting task 11

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,21 @@
+#!/us/bin/python3
+"""
+Starting Flask application
+"""
+
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.route('/hbnb_filters', strict_slashes=False)
+def hbnb_filters():
+    """ Implmenting filter """
+    states = storage.all('State')
+    amenities = storage.all('Amenity')
+    return render_template('10-hbnb_filters.html', states=states, amenities=amenities)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
