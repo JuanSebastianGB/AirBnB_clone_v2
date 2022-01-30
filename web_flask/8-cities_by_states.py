cfe449cb55d583723f46f4998b35a03ba19ea403
JuanSebastianGB <3393@holbertonschool.com>
2022-01-30 02:44:11 +0000

    STYLE: acoomplish some peep 8 requirements

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -12,7 +12,8 @@ app = Flask(__name__)
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
     """ Rendering template to show states list """
-    return render_template('8-cities_by_states.html', states=storage.all('State'))
+    return render_template('8-cities_by_states.html',
+                           states=storage.all('State'))
 
 
 @app.teardown_appcontext
