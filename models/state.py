c1bf643de392b3d81d62534abb3ea290dda54d1b
JuanSebastianGB <3393@holbertonschool.com>
2022-01-30 01:18:59 +0000

    UPL: Uploading task 7, by showing list of states

--- models/state.py
+++ models/state.py
@@ -13,7 +13,10 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
 
-    if getenv('HBNB_TYPE_STORAGE') != 'db':
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship('City', backref='state', cascade='delete')
+    else:
         @property
         def cities(self):
             """Get cities.
