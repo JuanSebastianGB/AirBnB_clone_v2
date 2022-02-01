a5e6d1bb413b02435f4802cd2a76e7b870606811
JuanSebastianGB <3393@holbertonschool.com>
2022-02-01 02:40:24 +0000

    FIX: Fixing if condition

--- models/place.py
+++ models/place.py
@@ -6,36 +6,47 @@ import models
 from models.amenity import Amenity
 from models.review import Review
 from models.base_model import Base, BaseModel
-import os
+from os import getenv
 
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    place_amenity = Table('place_amenity', Base.metadata,
+                          Column('place_id', String(60), ForeignKey('places.id'),
+                                 primary_key=True, nullable=False),
+                          Column('amenity_id', String(60),
+                                 ForeignKey('amenities.id'),
+                                 primary_key=True, nullable=False))
 
-place_amenity = Table('place_amenity', Base.metadata,
-                      Column('place_id', String(60), ForeignKey('places.id'),
-                             primary_key=True, nullable=False),
-                      Column('amenity_id', String(60),
-                             ForeignKey('amenities.id'),
-                             primary_key=True, nullable=False))
+    class Place(BaseModel, Base):
+        """ A place to stay """
+        __tablename__ = "places"
+        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        description = Column(String(1024))
+        number_rooms = Column(Integer, default=0, nullable=False)
+        number_bathrooms = Column(Integer, default=0, nullable=False)
+        max_guest = Column(Integer, default=0, nullable=False)
+        price_by_night = Column(Integer, default=0, nullable=False)
+        latitude = Column(Float)
+        longitude = Column(Float)
 
-
-class Place(BaseModel, Base):
-    """ A place to stay """
-    __tablename__ = "places"
-    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    name = Column(String(128), nullable=False)
-    description = Column(String(1024))
-    number_rooms = Column(Integer, default=0, nullable=False)
-    number_bathrooms = Column(Integer, default=0, nullable=False)
-    max_guest = Column(Integer, default=0, nullable=False)
-    price_by_night = Column(Integer, default=0, nullable=False)
-    latitude = Column(Float)
-    longitude = Column(Float)
-
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
         reviews = relationship('Review', backref='place', cascade='delete')
         amenities = relationship('Amenity', secondary=place_amenity,
                                  overlaps="place_amenities", viewonly=False)
-    else:
+else:
+    class Place(BaseModel):
+        city_id = ''
+        user_id = ''
+        name = ''
+        description = ''
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.0
+        longitude = 0.0
+        amenity_ids = []
+
         @property
         def reviews(self):
             """Get reviews
@@ -63,5 +74,5 @@ class Place(BaseModel, Base):
             Args:
                 value ([Amenity]): amenity to be appended
             """
-            if value.__class__.__name__ == Amenity:
+            if value.__class__.__name__ == 'Amenity':
                 self.amenities.append(value)
--- models/review.py
+++ models/review.py
@@ -13,6 +13,7 @@ if getenv('HBNB_TYPE_STORAGE') == 'db':
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
         text = Column(String(1024), nullable=False)
 else:
-    place_id = ""
-    user_id = ""
-    text = ""
+    class Review(BaseModel):
+        place_id = ""
+        user_id = ""
+        text = ""
