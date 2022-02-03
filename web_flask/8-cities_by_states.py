d2274f42752d368bfef0518a855f0a68b1ae3d68
JuanSebastianGB <3393@holbertonschool.com>
2022-02-03 02:17:04 +0000

    UPD: Including prev task

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -5,15 +5,22 @@ Start Flask web application
 from models import storage
 from flask import Flask
 from flask import render_template
-
+from models.state import State
 app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """ Rendering template to show states list """
+    return render_template('7-states_list.html',
+                           states=storage.all('State'))
 
 
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
     """ Rendering template to show cities list by states  """
-    return render_template('8-cities_by_states.html',
-                           states=storage.all('State'))
+    states = storage.all(State)
+    return render_template('8-cities_by_states.html', states=states)
 
 
 @app.teardown_appcontext
@@ -23,4 +30,4 @@ def teardown_session(exc):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)
