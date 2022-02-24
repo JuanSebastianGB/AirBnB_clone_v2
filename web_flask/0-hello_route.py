14b725fcde44bbbd649cd7164bdd41b172b36c8e
JuanSebastianGB <3393@holbertonschool.com>
2022-02-24 03:20:22 +0000

    UPD: Deleting funciton to test

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -15,13 +15,5 @@ def index():
     return 'Hello HBNB!'
 
 
-@app.route('/airbnb-onepage', strict_slashes=False)
-def onepage():
-    """
-    Defining one-route
-    """
-    return "Hello HBNB!"
-
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
